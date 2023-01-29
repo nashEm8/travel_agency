d790dbca6a969efebb009880d8073bacd2837286
nashEm8 <emanuel82022@outlook.com>
2023-01-28 23:26:34 -0300

    Seção de destinos

---
+++ src/components/destinos/Destino.jsx
@@ -0,0 +1,72 @@
+import '../destinos/destino.css';
+import destino1 from '../../images/destino1.jpg'
+
+const Destino = () => {
+    return(
+        <>
+            <div className='text'>
+                <p>Destinos</p>
+                <h2>Escolha seu destino</h2>
+            </div>
+
+            <div className='options'>
+                <div className='option'>
+                    <img src={destino1}/>
+                    <p>Passeios & viagens</p>
+                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    <a href='#'>Descrição ></a>
+                </div>
+                <div className='option'>
+                    <img src={destino1}/>
+                    <p>Passeios & viagens</p>
+                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    <a href='#'>Descrição ></a>
+                </div>
+                <div className='option'>
+                    <img src={destino1}/>
+                    <p>Passeios & viagens</p>
+                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    <a href='#'>Descrição ></a>
+                </div>
+                <div className='option'>
+                    <img src={destino1}/>
+                    <p>Passeios & viagens</p>
+                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    <a href='#'>Descrição ></a>
+                </div>
+                <div className='option'>
+                    <img src={destino1}/>
+                    <p>Passeios & viagens</p>
+                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    <a href='#'>Descrição ></a>
+                </div>
+                <div className='option'>
+                    <img src={destino1}/>
+                    <p>Passeios & viagens</p>
+                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    <a href='#'>Descrição ></a>
+                </div>
+                <div className='option'>
+                    <img src={destino1}/>
+                    <p>Passeios & viagens</p>
+                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    <a href='#'>Descrição ></a>
+                </div>
+                <div className='option'>
+                    <img src={destino1}/>
+                    <p>Passeios & viagens</p>
+                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    <a href='#'>Descrição ></a>
+                </div>
+                <div className='option'>
+                    <img src={destino1}/>
+                    <p>Passeios & viagens</p>
+                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    <a href='#'>Descrição ></a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Destino; 
