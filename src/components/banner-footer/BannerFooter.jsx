f97c27d8e76c48cb59d754e80b0fc52ea482a439
nashEm8 <emanuel82022@outlook.com>
2023-01-30 20:52:13 -0300

    Banner próximo ao footer

---
+++ src/components/banner-footer/BannerFooter.jsx
@@ -0,0 +1,17 @@
+import '../banner-footer/bannerFooter.css';
+
+const BannerFooter = () => {
+    return(
+        <div className='banner-footer'>
+            <div className='text-banner-footer'>
+                <p>Comece Suas Aventuras</p>
+                <h2>Vamos Explorar Este Mundo</h2>
+                <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry 
+                    printing and typesetting.</span>
+                <a href='#'>Agende agora</a>
+            </div>
+        </div>
+    );
+}
+
+export default BannerFooter;
