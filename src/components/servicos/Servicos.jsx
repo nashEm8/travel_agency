cf180ec8214af42f29ae97d63f422c06f17d27a9
nashEm8 <emanuel82022@outlook.com>
2023-02-14 21:54:47 -0300

    Alterando largura

--- src/components/servicos/Servicos.jsx
+++ src/components/servicos/Servicos.jsx
@@ -2,65 +2,65 @@ import '../servicos/servicos.css';
 import { FaHotel, FaUtensils, FaRegClock, FaDollarSign, FaTheaterMasks, FaGlobe, FaHeadphones } from 'react-icons/fa';
 
 const Servicos = () => {
-    return(
+    return (
         <div className='conteudo-servicos'>
-        <div className='text'>
-            <p>Serviços</p>
-            <h2>Incontáveis experiências</h2>
-        </div>
+            <div className='text'>
+                <p>Serviços</p>
+                <h2>Incontáveis experiências</h2>
+            </div>
 
-        <div className='options-servicos'>
-            <div className='option-servicos'>
-                <FaHotel/>
-                <div className='text-servicos'>
-                    <p>Passeios & viagens</p>
-                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+            <div className='options-servicos'>
+                <div className='option-servicos'>
+                    <FaHotel />
+                    <div className='text-servicos'>
+                        <p>Passeios & viagens</p>
+                        <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    </div>
                 </div>
-            </div>
-            <div className='option-servicos' style={{marginLeft : '20px'}}>
-                <FaUtensils/>
-                <div className='text-servicos'>
-                    <p>Passeios & viagens</p>
-                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                <div className='option-servicos'>
+                    <FaUtensils />
+                    <div className='text-servicos'>
+                        <p>Passeios & viagens</p>
+                        <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    </div>
                 </div>
-            </div>
-            <div className='option-servicos' style={{marginLeft : '20px'}}>
-                <FaRegClock/>
-                <div className='text-servicos'>
-                    <p>Passeios & viagens</p>
-                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                <div className='option-servicos'>
+                    <FaRegClock />
+                    <div className='text-servicos'>
+                        <p>Passeios & viagens</p>
+                        <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    </div>
                 </div>
-            </div>
-            <div className='option-servicos' style={{marginLeft : '20px'}}>
-                <FaDollarSign/>
-                <div className='text-servicos'>
-                    <p>Passeios & viagens</p>
-                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                <div className='option-servicos'>
+                    <FaDollarSign />
+                    <div className='text-servicos'>
+                        <p>Passeios & viagens</p>
+                        <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    </div>
                 </div>
-            </div>
-            <div className='option-servicos' style={{margin : '20px 0 0 0'}}>
-                <FaTheaterMasks/>
-                <div className='text-servicos'>
-                    <p>Passeios & viagens</p>
-                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                <div className='option-servicos'>
+                    <FaTheaterMasks />
+                    <div className='text-servicos'>
+                        <p>Passeios & viagens</p>
+                        <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    </div>
                 </div>
-            </div>
-            <div className='option-servicos' style={{margin : '20px 0 0 20px'}}>
-                <FaGlobe/>
-                <div className='text-servicos'>
-                    <p>Passeios & viagens</p>
-                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                <div className='option-servicos'>
+                    <FaGlobe />
+                    <div className='text-servicos'>
+                        <p>Passeios & viagens</p>
+                        <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    </div>
                 </div>
-            </div>
-            <div className='option-servicos' style={{margin : '20px 0 0 20px'}}>
-                <FaHeadphones/>
-                <div className='text-servicos'>
-                    <p>Passeios & viagens</p>
-                    <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                <div className='option-servicos'>
+                    <FaHeadphones />
+                    <div className='text-servicos'>
+                        <p>Passeios & viagens</p>
+                        <span>Lorem Ipsum is simply dummy text of the printing.</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 }
 
